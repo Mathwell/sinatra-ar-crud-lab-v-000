cbfa39bf6cc8ecf3fc6d51a31b81f0a802ad2907
Olena Ageyeva <eageeva@hotmail.com>
2017-12-22 00:41:28 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -2,4 +2,12 @@
 class Post<ActiveRecord::Base
   attr_accessor :name, :content
 
+  def initialize(name, content)
+    @name, @content=name,content
+  end
+
+  def create(params)
+    self.new(params[:post_name], params[:post_content])
+  end
+
 end
