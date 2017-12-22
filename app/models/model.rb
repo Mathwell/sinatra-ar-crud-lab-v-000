c44d6700ad05b7004731a985141159d9b7506b08
Olena Ageyeva <eageeva@hotmail.com>
2017-12-22 00:33:39 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,5 +1,8 @@
 #Placeholder for a model
 class Post<ActiveRecord::Base
   attr_accessor :name, :content
+  def initialize(params)
+    @name, @content =params[:name], params[:content]
+  end
 
 end
